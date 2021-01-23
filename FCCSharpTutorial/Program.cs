9a49e2c17253bd701519277f2bdc98cd98f94828
Sam Mudick <smudick@gmail.com>
2021-01-23 10:58:58 -0600

    string methods

--- FCCSharpTutorial/Program.cs
+++ FCCSharpTutorial/Program.cs
@@ -5,21 +5,31 @@ namespace FCCSharpTutorial
     class Program
     {
         static void Main(string[] args)
-        {
-            string phrase = "Giraffe Academy";
-            char grade = 'A';
-            int age = 30;
-            double gpa = 3.3;
-            bool isMale = true;
-            string name = "Tom";
-            int age = 25;
+        {   ///// Data Types
 
-            Console.WriteLine($"There once was a man named {name}");
-            Console.WriteLine($"He was {age} years old");
+            //string phrase = "Giraffe Academy";
+            //char grade = 'A';
+            //int age = 30;
+            //double gpa = 3.3;
+            //bool isMale = true;
 
-            name = "mike";
-            Console.WriteLine($"He really liked the name {name}");
-            Console.WriteLine($"But he didn't like being {age}");
+
+            ////// Variables
+            ///
+            //string name = "Tom";
+            //int age = 25;
+
+            //Console.WriteLine($"There once was a man named {name}");
+            //Console.WriteLine($"He was {age} years old");
+
+            //name = "mike";
+            //Console.WriteLine($"He really liked the name {name}");
+            //Console.WriteLine($"But he didn't like being {age}");
+
+            ////// Strings
+            string phrase = "\"Giraffe\" \nAcademy";
+            Console.WriteLine(phrase.Substring(phrase.IndexOf("Academy")));
+            Console.WriteLine(phrase.Substring(3, 4));
         }
     }
 }
